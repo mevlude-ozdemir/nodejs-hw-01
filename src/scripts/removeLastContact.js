2ede54e1b268f50b8f3724d39811370a8805f4d2
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 06:05:32 +0300

    step 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,22 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+    if (contacts.length === 0) {
+      console.log('Silinecek kişi bulunamadı. Dizi zaten boş.');
+      return;
+    }
+
+    const removed = contacts.pop();
+    await writeContacts(contacts);
+
+    console.log('Son kişi silindi:', removed);// silinen kişiyi console a yazadır
+  } catch (err) {
+    console.error('Son kişiyi silerken hata oluştu:', err);
+  }
+};
 
 removeLastContact();
