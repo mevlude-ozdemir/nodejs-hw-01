a8c160d5be112c635dfbc68f736149a420898524
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 05:34:35 +0300

    step 5

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,15 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+
+    return contacts;
+  } catch (err) {
+    console.error('Dosya ve dizin listesini alma hatası:', err);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
