3f41a1b397d439d5ac96622991d784bf6826f1ba
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 05:55:56 +0300

    step 7

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,15 @@
-export const removeAllContacts = async () => {};
+import fs from "fs/promises";
+import { PATH_DB } from "../constants/contacts.js";
+
+export const removeAllContacts = async () => {
+   try {
+    
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), "utf8");
+    console.log("Tüm kişiler başarıyla silindi.");
+  } catch (err) {
+    console.error("Kişileri silerken hata oluştu:", err);
+  }
+
+};
 
 removeAllContacts();
