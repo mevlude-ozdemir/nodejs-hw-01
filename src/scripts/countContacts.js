90afd589c0ee2a24f299eefd16a1fac54b655993
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-09-18 05:49:39 +0300

    step 6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,25 @@
-export const countContacts = async () => {};
+
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+
+try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+
+  if (Array.isArray(contacts)) {
+      return contacts.length;
+    } else {
+      console.error("Veri dizi değil!");
+      return 0;
+    }
+
+} catch (err) {
+    console.error("Dosya okuma hatası:", err);
+    return 0;
+  }
+
+
+};
 
 console.log(await countContacts());
